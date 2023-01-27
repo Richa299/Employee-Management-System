b87655aa9f9f7f46769b00c9ae2bca2b3f625264
Richa299 <richa.singhnxt213@gmail.com>
2023-01-28 00:37:38 +0530

    Add employee form has been created

--- src/Pages/Dashboard/Add.jsx
+++ src/Pages/Dashboard/Add.jsx
@@ -1,6 +1,82 @@
-function Add(){
+import { useState } from "react";
+import Swal from "sweetalert2";
+import { employeesData } from "../../Data";
+function Add({setIsAdding,empList,setEmpList}){
+    const [firstName,setFirstName]=useState('');
+    const [lastName,setLastName]=useState('');
+    const [email,setEmail]=useState('');
+    const [address,setAddress]=useState('');
+    const [salary, setSalary]=useState('');
+    const [date,setDate]=useState('');
+    const handleAdd=(e)=>{
+        e.preventDefault();
+        if(!firstName || !lastName || !email || !address || !salary || !date){
+              return Swal.fire({
+                icon:"error",
+                title:"Error!",
+                text:"All field are required"
+              }
+              )
+        }
+        const id=empList.length+1;
+        const newEmployee={
+            id,
+            firstName,
+            lastName,
+            email,
+            address,
+            salary,
+            date
+        }
+       empList.push(newEmployee)
+       setEmpList(empList)
+       setIsAdding(false)
+    }
+    
     return(
-        <>Add</>
+        <div className="w-5/6 mx-auto">
+            <div className="header-part bg-green-500 w-148 h-16">
+            <h3 className="font-bold text-2xl pt-3 text-center">Add Employee</h3>
+            </div>
+            <form className="text-left ml-4 p-4 flex flex-col mt-3">
+            <label className="label-inputs">First Name</label>
+            <input 
+                 type="text" 
+                 className="input-items" 
+                 onChange={e=>setFirstName(e.target.value)}/>
+            <label className="label-inputs">Last Name</label>
+            <input 
+                 type="text" 
+                 className="input-items" 
+                 onChange={e=>setLastName(e.target.value)}/>
+            <label className="label-inputs">Email</label>
+            <input 
+                 type="email" 
+                 className="input-items"
+                 onChange={e=>setEmail(e.target.value)} />
+            <label className="label-inputs">Address</label>
+            <input 
+                  type="text" 
+                  className="input-items" 
+                  onChange={e=>setAddress(e.target.value)}/>
+            <div>
+            <label className="label-inputs">Salary</label>
+            <input 
+                  type="number" 
+                  className="salary" 
+                  onChange={e=>setSalary(e.target.value)}/>
+            <label className="label-inputs-dat">Date</label>
+            <input 
+                  type="date" 
+                  className="date"
+                  onChange={e=>setDate(e.target.value)} />
+            </div>
+            <div>
+            <button className="add-button" onClick={handleAdd}>Add</button>
+            <button className="cancel-button" onClick={()=>{setIsAdding(false)}}>Cancel</button>
+            </div>
+            </form>
+        </div>
     )
-}
+};
 export default Add;
