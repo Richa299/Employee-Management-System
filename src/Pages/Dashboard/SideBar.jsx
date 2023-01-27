efe2eb3392faa744f946c40d0aca8dcbfbd08a41
Richa299 <richa.singhnxt213@gmail.com>
2023-01-28 00:38:42 +0530

    sidebar created for navigation

--- src/Pages/Dashboard/SideBar.jsx
+++ src/Pages/Dashboard/SideBar.jsx
@@ -1,18 +1,27 @@
 import {IoPersonAddSharp} from 'react-icons/io5'
 import {CgHome} from 'react-icons/cg'
-import {GrProjects} from 'react-icons/gr'
-import {BsFillBarChartFill} from 'react-icons/bs'
-function SideBar(){
+import {BsFillBarChartFill, BsFillGridFill} from 'react-icons/bs'
+import {AiFillSetting} from 'react-icons/ai'
+function SideBar({setIsAdding}){
     return(
-        <div className="container fixed top-0 h-screen w-32 bg-yellow-500">
-        <ul className="ml-4 mt-32 space-y-4">
-            <li><CgHome size="1rem"/> </li>
-            <li><IoPersonAddSharp size="1rem"/> </li>
-            <li><GrProjects size="1rem"/> </li>
-            <li><BsFillBarChartFill size="1rem"/> </li>
-            <li><IoPersonAddSharp size="1rem"/> </li>
+        <div className='sidebar-expand flex w-24 bg-yellow-500 h-screen text-black-900 text-0.5'>
+        <ul className="mt-28">
+            <SideBarIcon icon={<CgHome/>} text={"Dashboard"}/>
+            <SideBarIcon icon={<IoPersonAddSharp/> } text={"Add Employee"} setIsAdding={setIsAdding} />
+            <SideBarIcon icon={<BsFillGridFill/>} text={"Projects"}/>
+            <SideBarIcon icon={<BsFillBarChartFill/>} text={"Reports"}/>
+            <SideBarIcon icon={<AiFillSetting/>} text={"Settings"}/>
         </ul>
         </div>
     )
 }
+const SideBarIcon=({icon, text={text} ,setIsAdding})=>(
+<div className='sidebar-icon'>
+  <p className='flex group'>
+     <span className='mt-1'  onClick={()=>setIsAdding(true)}>{icon}</span>
+     <span className='tool group-hover:scale-100' onClick={()=>setIsAdding(true)}>{text}</span>
+   </p>
+</div>
+);
+
 export default SideBar
